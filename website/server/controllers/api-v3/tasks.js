044f174f13fc6c17fc4ed847d147c346b518ed21
Tyler Nychka <Tyler.Nychka@gmail.com>
2019-08-10 14:45:02 -0400

    Documentation Bug Fix

--- website/server/controllers/api-v3/tasks.js
+++ website/server/controllers/api-v3/tasks.js
@@ -45,7 +45,7 @@ function canNotEditTasks (group, user, assignedUserId) {
 
 /**
  * @apiDefine NotAuthorized
- * @apiError (401) {NotAuthorized} There is no account that uses those credentials.
+ * @apiError (401) {NotAuthorized} NotAuthorized There is no account that uses those credentials.
  */
 
 let api = {};
@@ -139,7 +139,7 @@ let requiredGroupFields = '_id leader tasksOrder name';
  * @apiError (400) {BadRequest} Text-ValidationFailed Path 'text' is required.
  * @apiError (400) {BadRequest} Alias-ValidationFailed Task short names can only contain alphanumeric characters, underscores and dashes.
  * @apiError (400) {BadRequest} Value-ValidationFailed `x` is not a valid enum value for path `(body param)`.
- * @apiError (401) {NotAuthorized} There is no account that uses those credentials.
+ * @apiError (401) {NotAuthorized} NotAuthorized There is no account that uses those credentials.
  *
  * @apiErrorExample {json} Error-Response:
  *     {
@@ -225,7 +225,7 @@ api.createUserTasks = {
  * @apiError (400) {BadRequest} Text-ValidationFailed Path 'text' is required.
  * @apiError (400) {BadRequest} Alias-ValidationFailed Task short names can only contain alphanumeric characters, underscores and dashes.
  * @apiError (400) {BadRequest} Value-ValidationFailed `x` is not a valid enum value for path `(body param)`.
- * @apiError (401) {NotAuthorized} There is no account that uses those credentials.
+ * @apiError (401) {NotAuthorized} NotAuthorized There is no account that uses those credentials.
  */
 api.createChallengeTasks = {
   method: 'POST',
@@ -278,8 +278,8 @@ api.createChallengeTasks = {
  * @apiSuccessExample
  * {"success":true,"data":[{"_id":"8a9d461b-f5eb-4a16-97d3-c03380c422a3","userId":"b0413351-405f-416f-8787-947ec1c85199","text":"15 minute break","type":"reward","notes":"","tags":[],"value":10,"priority":1,"attribute":"str","challenge":{},"group":{"assignedUsers":[],"approval":{"required":false,"approved":false,"requested":false}},"reminders":[],"createdAt":"2017-01-07T17:52:09.121Z","updatedAt":"2017-01-11T14:25:32.504Z","id":"8a9d461b-f5eb-4a16-97d3-c03380c422a3"},,{"_id":"84c2e874-a8c9-4673-bd31-d97a1a42e9a3","userId":"b0413351-405f-416f-8787-947ec1c85199","alias":"prac31","text":"Practice Task 31","type":"daily","notes":"","tags":[],"value":1,"priority":1,"attribute":"str","challenge":{},"group":{"assignedUsers":[],"approval":{"required":false,"approved":false,"requested":false}},"reminders":[{"time":"2017-01-13T16:21:00.074Z","startDate":"2017-01-13T16:20:00.074Z","id":"b8b549c4-8d56-4e49-9b38-b4dcde9763b9"}],"createdAt":"2017-01-13T16:34:06.632Z","updatedAt":"2017-01-13T16:49:35.762Z","checklist":[],"collapseChecklist":false,"completed":true,"history":[],"streak":1,"repeat":{"su":false,"s":false,"f":true,"th":true,"w":true,"t":true,"m":true},"startDate":"2017-01-13T00:00:00.000Z","everyX":1,"frequency":"weekly","id":"84c2e874-a8c9-4673-bd31-d97a1a42e9a3"}],"notifications":[]}
  *
- * @apiError (BadRequest) Invalid_request_parameters Error returned if the type URL param was not correct.
- * @apiError (401) {NotAuthorized} There is no account that uses those credentials.
+ * @apiError (400) {BadRequest} Invalid_request_parameters Error returned if the type URL param was not correct.
+ * @apiError (401) {NotAuthorized} NotAuthorized There is no account that uses those credentials.
  */
 api.getUserTasks = {
   method: 'GET',
